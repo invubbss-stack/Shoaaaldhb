3b178611b541b2d5b0a715ea4189290db23e13b1
invubbss-stack <invubbss@gmail.com>
2026-07-18 08:39:43 +0300

    Redesign premium digital menu with JSON data

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'rukn-al-jaleed-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data/categories.json',
+  './data/products.json',
+  './data/offers.json',
+  './data/settings.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
